cc1a84fff67d4c049281f9f631f70d462fa850f9
Harry Cooper <hgc17@ic.ac.uk>
2021-04-10 12:21:47 +0100

    Initial commit

---
+++ highway_example.py
@@ -0,0 +1,59 @@
+# Environment
+import gym
+import highway_env
+
+# Agent
+from rl_agents.trainer.evaluation import Evaluation
+from rl_agents.agents.common.factory import agent_factory
+from rl_agents.agents.common.factory import load_agent, load_environment
+
+# Visualisation
+from tqdm import trange
+
+# Profiling 
+import cProfile
+import pstats
+
+"""
+Notes:
+- What does "expanding terminal states" mean?
+- What does the DeterministicPlannnerAgent really do?
+- Informally, which configuration options affect the runtime the most?
+- How can we then formally measure this?
+- How do we train a model?
+
+Profiling Tools
+- pycallgraph
+- cProfile
+- line_profiler
+- pympler
+"""
+
+# Make agent and env
+env_config = "configs/HighwayEnv/env.json"
+agent_config = "configs/HighwayEnv/agents/DQNAgent/ddqn.json"
+env = load_environment(env_config)
+agent = load_agent(agent_config, env)
+evaluation = Evaluation(env, agent, num_episodes=10, display_env=False, display_agent=False, display_rewards=False)
+
+# Set up profiling
+pr = cProfile.Profile()
+pr.enable()
+
+# Train
+evaluation.train()
+
+# Save profiling result
+pr.disable()
+with open("profilingStatsAsText.txt", "w") as f:
+    ps = pstats.Stats(pr, stream=f)
+    ps.sort_stats('cumulative')
+    ps.print_stats()
+
+
+env = record_videos(env)
+
+# Test
+evaluation = Evaluation(env, agent, num_episodes=3, recover=True)
+evaluation.test()
+env.close()
