063353b6572271607d3af65c8cc0fc8d5cb52e9c
harry <harry@smartmine.net>
2021-05-03 23:34:25 +0100

    Update experiment script

--- marl_scalability/experiment.sh
+++ marl_scalability/experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-Algos="sac ppo dqn bdqn td3"
-LogDir="profile_3"
+Algos="sac"
+LogDir="large_profile"
 for algo in $Algos; do
-	python marl_scalability/train.py --task 0-4agents --level no-traffic --policy $algo,$algo,$algo,$algo --headless True --episodes 10 --eval-rate 1000000 --log-dir $LogDir
-	python marl_scalability/train.py --task 0-3agents --level no-traffic --policy $algo,$algo,$algo --headless True --episodes 10 --eval-rate 1000000 --log-dir $LogDir
-	python marl_scalability/train.py --task 0-2agents --level no-traffic --policy $algo,$algo --headless True --episodes 10 --eval-rate 1000000 --log-dir $LogDir
-	python marl_scalability/train.py --task 0-1agents --level no-traffic --policy $algo --headless True --episodes 10 --eval-rate 1000000 --log-dir $LogDir
+ 	python marl_scalability/train.py --scenario scenarios/loop_no_sv --policy $algo --headless --memprof --episodes 1000 --log-dir $LogDir --profiler cProfile --n-agents 1
+	for ((i=5; i<=50; i+=5)); do
+    		python marl_scalability/train.py --scenario scenarios/loop_no_sv --policy $algo --headless --memprof --episodes 1000 --log-dir $LogDir --profiler cProfile --n-agents $i
+	done
 done
