48f0541aba37ab8e9f5471d651c052c77a76054a
Harry Cooper <hgc17@ic.ac.uk>
2021-05-04 18:04:49 +0100

    Add updated baseline state preprocessor

--- marl_scalability/marl_scalability/baselines/common/baseline_state_preprocessor.py
+++ marl_scalability/marl_scalability/baselines/common/baseline_state_preprocessor.py
@@ -58,13 +58,13 @@ class BaselineStatePreprocessor(StatePreprocessor):
         return {
             "low_dim_states": {
                 "speed": 1,
-                #"distance_from_center": 1,
+                "distance_from_center": 1,
                 "steering": 1,
-                #"angle_error": 1,
+                "angle_error": 1,
                 #"relative_goal_position": 2,
                 # "action": int(action_size),  # 2
                 #"waypoints_lookahead": 2 * int(observation_waypoints_lookahead),
-                #"road_speed": 1,
+                "road_speed": 1,
             },
             "social_vehicles": int(social_vehicle_config["num_social_features"])
             if int(social_vehicle_config["social_capacity"]) > 0
@@ -83,7 +83,7 @@ class BaselineStatePreprocessor(StatePreprocessor):
         social_vehicle_config,
     ):
         state = self._adapt_observation_for_baseline(state)
-
+        
         # Obtain the next waypoints.
         '''
         _, lookahead_waypoints = self.extract_closest_waypoint(
@@ -144,9 +144,13 @@ class BaselineStatePreprocessor(StatePreprocessor):
         ego_speed = self.extract_ego_speed(state)
         ego_steering = self.extract_ego_steering(state)
         ego_start = self.extract_ego_start(state)
-        #ego_goal = self.extract_ego_goal(state)
-        #ego_waypoints = self.extract_ego_waypoints(state)
+        ego_goal = self.extract_ego_goal(state)
+        ego_waypoints = self.extract_ego_waypoints(state)
         social_vehicle_states = self.extract_social_vehicles(state)
+
+        ego_closest_waypoint = min(
+            ego_waypoints, key=lambda wp: wp.dist_to(ego_position)
+        )
 
         # Identify the path the ego is following.
         '''
@@ -165,13 +169,13 @@ class BaselineStatePreprocessor(StatePreprocessor):
         '''
 
         # Calculate the ego's distance from the center of the lane.
-        '''
+        
         signed_distance_from_center = ego_closest_waypoint.signed_lateral_error(
             ego_position
         )
-        '''
-        #lane_width = ego_closest_waypoint.lane_width * 0.5
-        #ego_distance_from_center = signed_distance_from_center / lane_width
+        
+        lane_width = ego_closest_waypoint.lane_width * 0.5
+        ego_distance_from_center = signed_distance_from_center / lane_width
 
         # Calculate the ego's relative, rotated position from the goal.
         '''
@@ -183,18 +187,18 @@ class BaselineStatePreprocessor(StatePreprocessor):
 
         basic_state = dict(
             speed=ego_speed,
+            distance_from_center=ego_distance_from_center,
             #relative_goal_position=ego_relative_rotated_goal_position,
-            #distance_from_center=ego_distance_from_center,
             steering=ego_steering,
-            #angle_error=ego_closest_waypoint.relative_heading(ego_heading),
+            angle_error=ego_closest_waypoint.relative_heading(ego_heading),
             social_vehicles=social_vehicle_states,
-            #road_speed=ego_closest_waypoint.speed_limit,
+            road_speed=ego_closest_waypoint.speed_limit,
             start=ego_start.position,
             #goal=ego_goal.position,
             heading=ego_heading,
             #goal_path=ego_goal_path,
             ego_position=ego_position,
-            #waypoint_paths=ego_waypoints,
+            waypoint_paths=ego_waypoints,
             events=state.events,
         )
         return basic_state
